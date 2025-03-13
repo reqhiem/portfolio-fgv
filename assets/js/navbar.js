859a10e6a4e5d83b12e26a8fab96e59666ff5923
Joel Perca <joelperca.vc@gmail.com>
2025-03-13 14:16:13 -0300

    fix: 🐛 jekyll compat

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,45 +8,10 @@ const createNavBar = () => {
   let nav = document.createElement("nav");
   nav.className = "header";
 
-  const isGitHubPages = location.hostname.includes("github.io");
-  let basePath = "";
-  let isSubfolder = false;
-
-  if (isGitHubPages) {
-    const pathParts = location.pathname.split("/").filter(Boolean);
-    if (pathParts.length > 0) {
-      const repoName = pathParts[0];
-      basePath = `/${repoName}`;
-
-      isSubfolder = pathParts.length > 1;
-    }
-  } else {
-    isSubfolder = location.pathname.split("/").filter(Boolean).length > 0;
-  }
-
-  console.log("Base path:", basePath);
-  console.log("Is subfolder:", isSubfolder);
-  
   for (let page of pages) {
     let a = document.createElement("a");
-    
-    const isHome = page.url === "";
-    const currentPath = location.pathname;
-    const isActive =
-      (isHome &&
-        (currentPath === basePath || currentPath === `${basePath}/`)) ||
-      (!isHome && currentPath.includes(page.url));
 
-    if (isActive) {
-      a.classList.add("active");
-    }
-    
-    if (isSubfolder) {
-      a.href = "../" + page.url;
-    } else {
-      a.href = page.url;
-    }
-    
+    a.href = siteConfig.baseUrl + "/" + page.url;
     a.innerText = page.title;
     nav.appendChild(a);
   }
---
+++ config.js
@@ -0,0 +1,3 @@
+const siteConfig = {
+  baseUrl: location.hostname.includes('github.io') ? '/portfolio-fgv' : ''
+};
